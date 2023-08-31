bd3fdeec02d4425c1c3e5c53c4984f683163fc52
lastra26 <73512238+lastra26@users.noreply.github.com>
2023-08-31 12:20:49 -0500

    resources del front 1
    resources del front 1

---
+++ resources/frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  base_url: '/api',
+  images_url: '/images/'
+};
---
+++ resources/frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  base_url: 'http://127.0.0.1:8000/api',
+  images_url: '/images/'
+};
